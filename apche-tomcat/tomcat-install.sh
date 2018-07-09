3fbb3886e4154d356c0ef3dfcce03ec46f6e5dc5
krishdd <krishdd@users.noreply.github.com>
2018-07-09 21:05:51 +0530

    initial commit

--- apche-tomcat/tomcat-install.sh
+++ apche-tomcat/tomcat-install.sh
@@ -1,1 +1,15 @@
-#!/bin/bash
+!/bin/bash
+#yum install java-1.8.0-openjdk
+java -version
+groupadd tomcat
+useradd -g tomcat -d /opt/tomcat -s /bin/nologin tomcat
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.32/bin/apache-tomcat-8.5.32.tar.gz
+tar -zxvf apache-tomcat*tar.gz
+mv apache-tomcat-8.5.32/* /opt/tomcat/
+chown -R tomcat:tomcat /opt/tomcat/
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --reload
+firewall-cmd --list-port
+sh /opt/tomcat/bin/startup.sh
+sh /opt/tomcat/bin/shutdown.sh
+sh /opt/tomcat/bin/startup.sh
